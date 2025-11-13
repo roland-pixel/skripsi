5c673ed5ebecfb69836a4a0cb364b54158e5b2bb
roland-pixel <kharis7181@gmail.com>
2025-11-13 22:36:04 +0700

    anjay

--- resources/views/halo.blade.php
+++ resources/views/halo.blade.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>halo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, dolorum. Iusto, sit ab. At nulla facere atque deleniti eaque minima odio laborum vel nihil! Nam, reprehenderit. Nam magnam mollitia possimus!</p>
 </body>
 </html>
